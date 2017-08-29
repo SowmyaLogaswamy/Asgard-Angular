d03541414b5649cbfd79e524a8eff957f995b02f
Alena Golovina and Sowmya Logaswamy <alena+sowmya@>
2017-08-29 11:27:19 -0700

    Add css styles to blendoku

--- src/app/blendoku/blendoku.component.ts
+++ src/app/blendoku/blendoku.component.ts
@@ -10,7 +10,10 @@ export class BlendokuComponent implements OnInit {
  ctnr1 = document;
  cubes: Cube[] = [
    new Cube("cube1"),
-   new Cube("cube2")
+   new Cube("cube2"),
+   new Cube("cube3"),
+   new Cube("cube4"),
+   new Cube("cube5")
    ];
   constructor() { }
 
